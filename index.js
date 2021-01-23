7f5f4ff6e0907b27ddd1870ca7c01f43a30c7656
Daniel <dmadera0@gmail.com>
2021-01-23 14:09:06 -0700

    final commit

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 require('dotenv').config();
 
 const Nylas = require('nylas')
-
+const fs = require('fs');
 
 const nylas = Nylas.with(process.env.Access_Token)
-
-
 
 Nylas.config({
     clientId: process.env.clientId,
@@ -14,55 +12,42 @@ Nylas.config({
 
 const draft = nylas.drafts.build({
     subject: "Nylas CC",
-    body: "Nylas interview tak home challenge",
-    to: [{name: "dantheMan", email:"dmadera0@gmail.com"}]
+    body: "Nylas interview take home challenge, Ready for review",
+    to: [{name: "Daniel Madera", email:"dmadera0@gmail.com"}]
 })
-
-module.exports = async (req, res) => {
-    const form = formidable({ multiples: true });
-  
-    return form.parse(req, async (error, fields, files) => {
-      if (error) {
-        return res.status(400).json({
-          error: "Failed to parse file upload."
-        });
-      }
-  
-      const upload = files.upload;
-  
-      if (!upload) {
-        return res.status(400).json({
-          error: "No file uploaded."
-        });
-      }
-  
-      const filename = upload.name;
-      const data = await fs.readFile(upload.path);
-      const contentType = upload.type;
-  
-      try {
-        const file = req.nylas.files.build({
-          filename: "./index.js",
-          data: data,
-          contentType
-        });
-  
-        await file.upload();
-  
-        res.json({
-          id: file.id,
-          filename: file.filename
-        });
-      } catch (error) {
-        console.log(error);
-        res.status(500).json({
-          error: "Failed to upload file to email account."
-        });
-      }
-    });
-  };
     
 draft.send().then(message => {
     console.log(`${message.body} was sent`);
-});    
+}); 
+
+// ///Attempt to upload as attachment for email///
+
+// ///file i want to send///
+// const filePath = "./index.js"
+
+
+// fs.readFile(filePath, function (err, data) {
+//     f = nylas.files.build({
+//         filename: filePath,
+//         data: data,
+//         contentType: 'text/plain'
+//     });
+
+
+// ///Uploading the file to Nylas API////
+//     f.upload(function(err, filePath) {
+//         // Create the draft and attach the file to it.
+//         const draft = nylas.drafts.build({
+//             subject: 'Nylas CC',
+//             to: [{name: "Daniel Madera", email: 'dmadera0@gmail.com'}],
+//             body: 'Hey, find the file attached.',
+//         });
+//         ////attaching the file
+//         draft.files = [filePath];
+
+//         ////console log is showing the draft it but not sending to email////
+//         draft.send().then(function(draft) {
+//             console.log(draft.id + ' was sent');
+//         });
+//     });
 
